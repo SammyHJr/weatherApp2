468bbf3ac1276b1233fce80e25d0857d5cacec04
Sammy H. jr <132605016+SammyHJr@users.noreply.github.com>
2025-01-27 10:24:10 +0100

    Initial Commit

---
+++ weatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Sam Hengami on 2025-01-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weatherAppTests/weatherAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  weatherAppTests.swift
+//  weatherAppTests
+//
+//  Created by Sam Hengami on 2025-01-27.
+//
+
+import Testing
+
+struct weatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
