176b541d880a638bfe46081e4cc80c22ca25df66
Sammy H. jr <Sam.Hengami@gmail.com>
2025-02-03 14:00:51 +0100

    Overarching design
    Here is the basic structure of how the  app will look

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -11,30 +11,120 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            Text("Sammy H. Jr")
+            Text("")
                 .containerRelativeFrame([.horizontal, .vertical])
                 .background(Gradient(colors: [.purple, .blue]))
-        
-        VStack{
+            
             VStack{
-                BoxView()
-                BoxView()
-                BoxView()
+                BoxView1()
+                sevenDayBoxView2()
+                BoxView3()
+            }
+        }
+    }
+}
+
+
+
+struct BoxView1: View {
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 300)
+                .opacity(0.3)
+                .padding(10)
+            
+            
+            VStack{
+                Text("(Weather). Highest (TEMP)C, Lowest(TEMP)C")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 12, weight: .bold))
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(30)
+            .padding(.bottom,250)
+            
+            VStack{
+                Text("TEMP")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+            }
+            .padding(30)
+            .padding(.bottom, 100)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack(alignment: .leading, spacing: 5) { // Adjust spacing as needed
+                Text("CITY")
+                    .fontWeight(.bold)
+                    .font(.system(size: 24))
+                Text("Day/Night")
+                Text("DAY, TIME")
+            }
+            .padding(30)
+            .padding(.top, 100)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundColor(.white)
+            
+            ZStack{
+                Text("SYMBOL")
+            }
+            .padding(30)
+            .padding(.top, 10)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .foregroundColor(.white)
+        }
+    }
+}
+
+//hourly forecast
+struct sevenDayBoxView2: View {
+    var body: some View{
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 100)
+                .opacity(0.3)
+                .padding(10)
+            
+            HStack{
+                ForEach(1...7, id: \.self) { i in
+                    Text("\(i)")
+                }
+                .foregroundColor(.white)
+                
+                
+            }
+            
+        }
+    }
+}
+
+//Will display the 7 day forecast
+struct BoxView3: View {
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 250)
+                .opacity(0.3)
+                .padding(10)
+            
+            VStack{
+                ForEach(1...7, id: \.self){ i in
+                    Text("\(i)")
                 }
             }
         }
     }
 }
 
-struct BoxView: View {
-    var body: some View{
-        RoundedRectangle(cornerRadius: 25)
-            .fill(.white)
-            .frame(width: 100, height: 100)
-            .opacity(0.5)
-        
-    }
-}
+
+
 
 //
 #Preview {
